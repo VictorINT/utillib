4af19a3cfaf00595b538bb51fe76ce0cd1ac597c
VictorINT <victor.s.florescu@gmail.com>
2021-01-02 21:36:07 +0200

    using pointer of vector, added `print_vector()` to header proprely

--- utillib.h
+++ utillib.h
@@ -1,10 +1,11 @@
 #ifndef UTILLIB_H_INCLUDED
 #define UTILLIB_H_INCLUDED
-
+#include <vector>
+using namespace std;
 bool primeStatus(int nr, int k);
 unsigned long long getNextPrimeNumber(unsigned long long nr);
 unsigned long long getPrevPrimeNumber(unsigned long long nr);
-void print_vector(vector <int> v1);
+void print_vector(vector <int> const& input)
 //void print_array(int *array, int length, int cols);
 
 #endif // VICTOR_UTILS_H_INCLUDED
