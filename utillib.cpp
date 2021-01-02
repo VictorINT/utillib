ac5a970a3dd8536e57fd8c9b22bc8b095317a3a8
VictorINT <victor.s.florescu@gmail.com>
2021-01-02 19:57:39 +0200

    primeStatus(), getNextPrimeNumber(), getPrevPrimeNumber(), print_vector()
    also print_array(), but it's not finished yet

---
+++ utillib.cpp
@@ -0,0 +1,130 @@
+#include "utillib.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+#define unsigned long long ULL //not using yet
+
+static long long power(unsigned long long x, unsigned long long  y, unsigned long long p)
+{
+    unsigned long long res = 1;
+    x = x % p;
+
+    while (y > 0)
+    {
+
+        if (y & 1)
+            res = (res * x) % p;
+
+
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+
+static bool miillerTest(unsigned long long d, unsigned long long n)
+{
+
+    unsigned long long a = 2 + rand() % (n - 4);
+
+    unsigned long long x = power(a, d, n);
+
+
+    if (x == 1 || x == n - 1)
+        return true;
+
+
+    while (d != n - 1)
+    {
+        x = (x * x) % n;
+       d *= 2;
+        //d = d << 1;
+        if (x == 1)      return false;
+        if (x == n - 1)    return true;
+    }
+
+
+    return false;
+}
+
+static bool isPrime(unsigned long long n, int k)
+{
+
+
+    if (n <= 3) return true;
+    if (n <= 1 || n == 4)  return false;
+
+
+
+    unsigned long long d = n - 1;
+    while (d % 2 == 0)
+        d /= 2;
+
+
+    for (int i = 0; i < k; i++)
+        if (!miillerTest(d, n))
+            return false;
+
+    return true;
+}
+
+
+bool primeStatus(int nr, int k)
+{
+    if ((nr & 0x1) && isPrime(nr, k))
+        return true;
+    else
+        return false;
+}
+
+unsigned long long getNextPrimeNumber(unsigned long long nr)
+{
+    if(primeStatus(nr))
+        nr++;
+    unsigned long long next = 1;
+    while(!primeStatus(next, 20)){
+        if(nr & 1){
+            next = nr;
+        }  else {
+            nr++;
+        }
+    }
+    return next;
+}
+
+unsigned long long getPrevPrimeNumber(unsigned long long nr)
+{
+    if(primeStatus(nr))
+        nr++;
+    unsigned long long prev;
+    while(!primeStatus(prev, 20)){
+
+        if((nr & 1) && ((nr-1) > 0)){
+            prev = nr;
+        }  else {
+            nr--;
+        }
+    }
+    return prev;
+}
+
+void print_vector(vector <int> v1){
+    printf('[');
+    for(int i = 0; i < (v1.size() - 1); i++){
+        printf(v1.at(i), ', ');
+    }
+    printf(v1.at(size() - 1),']');
+}
+//not done yet
+// void print_array(int *array, int length, int cols)
+// {
+//     for (int i = 0; i < length; i++) { 
+//         printf(); //number of dimensions ??
+//     }
+// }
---
+++ utillib.h
@@ -0,0 +1,10 @@
+#ifndef UTILLIB_H_INCLUDED
+#define UTILLIB_H_INCLUDED
+
+bool primeStatus(int nr, int k);
+unsigned long long getNextPrimeNumber(unsigned long long nr);
+unsigned long long getPrevPrimeNumber(unsigned long long nr);
+void print_vector(vector <int> v1);
+//void print_array(int *array, int length, int cols);
+
+#endif // VICTOR_UTILS_H_INCLUDED
