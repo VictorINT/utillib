4e1aab4ba25a6d1b0aec9f1017823a6090f95c44
Rares Vanca <raresvanca@gmail.com>
2021-01-03 14:40:25 +0200

    Fixed bugs

--- utillib.h
+++ utillib.h
@@ -1,13 +1,14 @@
 #ifndef UTILLIB_H_INCLUDED
 #define UTILLIB_H_INCLUDED
 
- #include <vector>
- using namespace std;
+#include "utillib.cpp"
+
+using namespace std;
 
 bool primeStatus(unsigned long long nr, int k);
 unsigned long long getNextPrimeNumber(unsigned long long nr);
 unsigned long long getPrevPrimeNumber(unsigned long long nr);
-void print_vector(vector<int> const& input)
+void print_vector(vector<int> const& input);
 //void print_array(int *array, int length, int cols);
 
 #endif // VICTOR_UTILS_H_INCLUDED
