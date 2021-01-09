97e08ef5cebaf9e96fd44c069a8ccb7c78b6090e
VictorINT <victor.s.florescu@gmail.com>
2021-01-09 11:54:16 +0200

    code cleanup(functions in progress eliminated)

--- utillib.cpp
+++ utillib.cpp
@@ -14,7 +14,6 @@ using namespace std;
 static long long power(unsigned long long x, unsigned long long y, unsigned long long p) {
   unsigned long long res = 1;
   x = x % p;
-
   while (y > 0) {
 
     if (y & 1)
@@ -38,7 +37,6 @@ static bool miillerTest(unsigned long long d, unsigned long long n) {
   while (d != n - 1) {
     x = (x * x) % n;
     d *= 2;
-    //d = d << 1;
     if (x == 1) return false;
     if (x == n - 1) return true;
   }
@@ -70,8 +68,6 @@ bool is_prime(unsigned long long nr, int k) {
 }
 
 unsigned long long get_next_prime_number(unsigned long long nr) {
-  // if(is_prime(nr, 20))
-  //     nr++;
   unsigned long long next = 0;
   while (!is_prime(next, 20)) {
     if (nr & 1) {
@@ -84,8 +80,6 @@ unsigned long long get_next_prime_number(unsigned long long nr) {
 }
 
 unsigned long long get_prev_prime_number(unsigned long long nr) {
-  // if(is_prime(nr, 20))
-  //     nr++;
   unsigned long long prev = 0;
   while (!is_prime(prev, 20)) {
 
@@ -104,52 +98,3 @@ void print_vector(vector <T> const & input) {
     input.end(),
     ostream_iterator <T> (cout, " "));
 }
-
-vector < long long > store_number(string number) {
-  vector < char > digits = {
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9
-  };
-  //key = number.at(0);
-  //for(int i = 1; i < number.size(); i++){
-  //     if(count(digits.begin(), number.end(), key)){
-  //       key = number.at(i);
-  //     } else {
-  //        return 0;
-  //    }
-  //}
-  vector < long long > numbers_stored;
-  stringstream ss(number);
-  //ss >> numbers_stored
-  //maybe break the ss into pieces until all of them are smaller then long long ??
-  // vector<char> redirect;
-  // long long t = 0;
-  // for(int i = 0; i < number.size(); i++){
-  //     if(redirect.size()*10 > 4294967295){
-  //         for(int j = 0; j < redirect.size(); j++){
-  //             t = t*10 + redirect.at(j);
-  //         }
-  //         numbers_stored.push_back(t);
-  //         t = 0;
-  //     } else {
-  //         redirect.push_back(number.at(i));
-  //     }
-  // }
-  return numbers_stored;
-}
-
-//not done yet
-// void print_array(int *array, int length, int cols)
-// {
-//     for (int i = 0; i < length; i++) { 
-//         printf(); //number of dimensions ??
-//     }
-// }
--- utillib.h
+++ utillib.h
@@ -9,7 +9,5 @@ bool is_prime(unsigned long long nr, int k);
 unsigned long long get_next_prime_number(unsigned long long nr);
 unsigned long long get_prev_prime_number(unsigned long long nr);
 template<typename T> void print_vector(vector <T> const & input);
-//void print_array(int *array, int length, int cols);
-//vector<long long> store_number(string number);
 
 #endif // VICTOR_UTILS_H_INCLUDED
